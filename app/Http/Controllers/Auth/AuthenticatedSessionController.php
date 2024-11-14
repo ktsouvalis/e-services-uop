6c8bd303da0d73839d85677190074683da2e8e2d
Konstantinos Tsouvalis <ktsouvalis@uop.gr>
2024-11-14 14:16:11 +0200

    install directorytree/ldaprecord, publish provider LdapRecord\Laravel\LdapServiceProvider, edit AuhtenticatedSessionController to match my new authentication, remove password from migrations, remove register from welcome blade

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\View\View;
+use LdapRecord\Container;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use LdapRecord\Models\OpenLDAP\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Context;
 use App\Http\Requests\Auth\LoginRequest;
@@ -24,14 +26,34 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(Request $request): RedirectResponse
     {
-        $request->authenticate();
+        // $request->authenticate();
+        // dd($request->all());
+        $username = $request->input('username');
+        $password = $request->input('password');
 
-        $request->session()->regenerate();
+        // Connect to the LDAP server
+        $ldap = Container::getDefaultConnection();
+        $ldap->connect();
+               
+        // Search for the user
+        $user = User::where('uid', '=', $username)->first();
 
-        Context::push('user', Auth::user()->email);
-        Context::push('user', Auth::user()->name);
+        if (!$user) {
+            return redirect()->back()->with('error', 'Invalid credentials');
+        }
+        
+        // Attempt to bind with the user's credentials
+        $isAuthenticated = $ldap->auth()->attempt($user, $password);
+        if($isAuthenticated){
+            auth()->login(\App\Models\User::where('username', $username)->firstOrFail());
+            session()->regenerate();
+        }
+        else{
+            return redirect()->back()->with('error', 'Invalid credentials');
+        }
+        
 
         Log::info('User logged in.');
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,32 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
+    @if(session()->has('error'))
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center" role="alert">
+                    <strong class="font-bold">Error!</strong>
+                    <span class="block sm:inline">{{ session('error') }}</span>
+                </div>
+            @endif
+            @if(session()->has('success'))
+                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative text-center" role="alert">
+                    <strong class="font-bold">Success!</strong>
+                    <span class="block sm:inline">{{ session('success') }}</span>
+                </div>
+            @endif
+            @if(session()->has('warning'))
+                <div class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative text-center" role="alert">
+                    <strong class="font-bold">Warning!</strong>
+                    <span class="block sm:inline">{{ session('warning') }}</span>
+                </div>
+            @endif
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -33,11 +50,11 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+            {{-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif --}}
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
